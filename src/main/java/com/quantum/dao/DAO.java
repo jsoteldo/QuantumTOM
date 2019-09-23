5b48e9d67cd1d59de5357e9d1ed2bad3867635d2
jsoteldo <georsot@gmail.com>
2019-09-23 15:54:26 -0500

    Update DAO.java
    cambios en el metedo de conexion respecto al host

--- src/main/java/com/quantum/dao/DAO.java
+++ src/main/java/com/quantum/dao/DAO.java
@@ -25,12 +25,24 @@ public class DAO {
             Properties prop = new Properties();
             System.out.println("\n\n=======================\nJDBC Connector Test ");
             System.out.println("User home directory: " + System.getProperty("user.home"));
-            Class.forName("org.mariadb.jdbc.Driver");
+            
+            prop.load(new java.io.FileInputStream(System.getProperty("user.home") + "/mydb.cfg"));
+            
+            
+            Class.forName(prop.getProperty("driver").toString());
+            
+            conexion = DriverManager.getConnection(prop.getProperty("host").toString(), prop.getProperty("username").toString(), prop.getProperty("password").toString());
+
+        
             //conexion = DriverManager.getConnection("jdbc:mariadb://192.168.3.17:3306/test", "flamencos", "$flamencosQuantum");
-            conexion = DriverManager.getConnection("jdbc:mariadb://node48103-env-3675608.jl.serv.net.mx/test", "flamencos", "$flamencosQuantum");
+            //conexion = DriverManager.getConnection("jdbc:mariadb://node48103-env-3675608.jl.serv.net.mx/test", "flamencos", "$flamencosQuantum");
 
         } catch (Exception e) {
             throw e;
+        }
+        catch (IOException e) {
+            System.out.println("Unable to find mydb.cfg in " + System.getProperty("user.home") + "\n Please make sure that configuration file created in this folder.");
+             
         }
     }
 
